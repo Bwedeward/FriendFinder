b9f4b4a5d29aefc3d52f24f500bec875eb2dccab
Brooke W <bwedeward@gmail.com>
2020-05-21 20:48:48 -0500

    add server.js functions

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 var express = require("express")
 var path = require("path")
+
+var app = express();
+
+var PORT = proces.env.PORT || 3333;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
