c180c0c6b1886c549c349a2271496922e78f3dd3
Brooke W <bwedeward@gmail.com>
2020-05-29 22:11:01 -0500

    inital heroku commit

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,11 @@ module.exports = function(app) {
     app.get("/home", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/home.html"));
     });
-  
+
+    // app.get("/", function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../public/home.html"));
+    // });
+
     app.get("/survey", function(req, res) {
       res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
